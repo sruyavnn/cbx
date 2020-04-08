6915a667cac7f9dce1f900e224d1461d590a0ece
suryavnn <surya.knlk@gmail.com>
2020-04-08 22:26:06 +0530

    profile service Integration

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _sharedservice: SharedService,) { }
 
   ngOnInit() {
+    this.getprofile();
   }
-
+  public userDetails;
+  getprofile(){
+    var url = '/otmmapi/v5/users/me';
+    this._sharedservice.getService(url).subscribe(data => {
+    this.userDetails=data.user_resource.user;
+    console.log(this.userDetails);
+    //this.showModal = true; // Show-Hide Modal Check
+  })
+  }
 }
